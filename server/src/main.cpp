9b6baec5a86fd1054174ef9b974c70fea3eef6ca
Jailior <ahusseinse@yahoo.com>
2025-05-13 20:21:05 -0400

    server added

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -2,38 +2,87 @@
 #include <fstream>
 #include <iostream>
 
-using namespace boost::asio::ip::tcp;
+using boost::asio::ip::tcp;
 
-#define PORT 12345
+const unsigned int PORT = 12345;
+
+std::string handler() {
+    return "hello";
+}
 
 int main() {
+
     try {
-    boost::asio::io_context io_context;
-    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), PORT));
+        boost::asio::io_context io_context;
 
-    std::cout << "Server is running on port " << PORT << std::endl;
+        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), PORT));
 
-    tcp::socket socket(io_context);
-    acceptor.accept(socket);
-    std::cout << "Client connected" << std::endl;
+        std::cout << "Server is running on port: " << PORT << std::endl;
 
-    uint32_t img_length;
-    boost::asio::read(socket, boost::asio::buffer(&img_length, sizeof(img_length)));
+        // for (;;) {
+            tcp::socket socket(io_context);
+            acceptor.accept(socket);
 
-    std::vector<uint8_t> buffer(img_length);
-    boost::asio::read(socket, boost::asio::buffer(buffer));
+            std::cout << "Client connected.\n";
 
+            std::string message = handler();
 
-    std::ofstream file("received_image.jpg", std::ios::binary);
-    file.write(reinterpret_cast<const char*>(buffer.data()), img_length);
-    file.close();
+            boost::system::error_code ignored_error;
+            uint32_t img_length;
+            boost::asio::read(socket, boost::asio::buffer(&img_length, sizeof(img_length)));
 
-    std::cout << "Image received and saved as received_image.jpg" << std::endl;
-    socket.close();
-    acceptor.close();
+            std::cout << "Got image length: " << img_length << std::endl;
+
+            std::vector<uint8_t> buffer(img_length);
+            boost::asio::read(socket, boost::asio::buffer(buffer, buffer.size()));
+
+            std::cout << "Read Image successfully.\n";
+
+            std::ofstream file("received_img.jpg", std::ios::binary);
+            file.write(reinterpret_cast<const char*>(buffer.data()), img_length);
+
+            std::cout << "Image received and saved as received_image.jpg" << std::endl;
+
+            file.close();
+            socket.close();
+            acceptor.close();
+
+        // }
     } catch (std::exception& e) {
-        std::cerr << "Exception: " << e.what() << std::endl;
+        std::cerr << e.what() << std::endl;
     }
+
 
     return 0;
 }
+
+
+
+
+ // try {
+    // boost::asio::io_context io_context;
+    // tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), PORT));
+
+    // std::cout << "Server is running on port " << PORT << std::endl;
+
+    // tcp::socket socket(io_context);
+    // acceptor.accept(socket);
+    // std::cout << "Client connected" << std::endl;
+
+    // uint32_t img_length;
+    // boost::asio::read(socket, boost::asio::buffer(&img_length, sizeof(img_length)));
+
+    // std::vector<uint8_t> buffer(img_length);
+    // boost::asio::read(socket, boost::asio::buffer(buffer));
+
+
+    // std::ofstream file("received_image.jpg", std::ios::binary);
+    // file.write(reinterpret_cast<const char*>(buffer.data()), img_length);
+    // file.close();
+
+    // std::cout << "Image received and saved as received_image.jpg" << std::endl;
+    // socket.close();
+    // acceptor.close();
+    // } catch (std::exception& e) {
+    //     std::cerr << "Exception: " << e.what() << std::endl;
+    // }
